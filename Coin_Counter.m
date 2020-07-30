a57cfb92d2fe3033ca4c531bf6da1082ff06635a
Harry Gordenstein <33324346+gordenstein72@users.noreply.github.com>
2020-07-30 14:37:31 -0400

    Coin_counter.m now has working circles and a very creud value calculation.

--- Coin_Counter.m
+++ Coin_Counter.m
@@ -1,39 +1,58 @@
-coin_pic = imread("photos/coin11.jpg");
+coins = imresize(imread("photos/good_coin.jpg"), .2);
+figure; subplot(1,3,1); imshow(coins); title('Original');
+bwCoins = im2double(rgb2gray(coins));
+subplot(1,3,2); imshow(bwCoins); title('Grayscale image');
 
-small_img = imresize(coin_pic,.25);
-% I = imresize(rgb2gray(coin_pic), 0.25);
-% coin_he = histeq(I, 5);
+%Find each coin in image using imfindcircles
+[centers, radii, metric] = imfindcircles(bwCoins, [30 90], 'ObjectPolarity','bright', 'Sensitivity',0.96, 'Method', 'TwoStage');
 
-I = rgb2gray(small_img);
+subplot(1,3,3); imshow(coins);
 
-%calculate Hough Transform
-BW = edge(I,'canny');
-[H,T,R] = hough(BW,'RhoResolution',0.5,'Theta',-90:0.5:89);
+%Show circles on original image
+detected = viscircles(centers, radii);
+drawnow;
 
-%display Hough Transform & rgb image
-% subplot(2,1,1);
-% imshow(coin_pic);
-% title('coin1.jpg');
-% subplot(2,1,2);
-% imshow(imadjust(rescale(H)),'XData',T,'YData',R,...
-%       'InitialMagnification','fit');
-% title('Hough transform of coin1.jpg');
-% xlabel('\theta'), ylabel('\rho');
-% axis on, axis normal, hold on;
-% colormap(gca,hot);
+title(sprintf('Number of Coins Detected: %d', size(centers,1)));
 
-%find circles 
-[centers, radii, metric] = imfindcircles(BW, [80,150]); %,'ObjectPolarity','bright'
-numCoin = length(centers);
-centersStrong5 = centers(1:numCoin); %centers is 11 elements after first run
-radiiStrong5 = radii(1:numCoin);
-metricStrong5 = metric(1:numCoin);
+%End early if no coins are detected
+if size(centers, 1) == 0
+    disp('No coins detected. Press any key to exit');
+    pause;
+    clc; close all; clear all;
+    return;
+end
 
-%display on image
-figure();
-%imshow(coin_pic);
-viscircles(centersStrong5, radiiStrong5,'EdgeColor','b');
+display(radii)
 
-coin_he = histeq(coin_pic, 50);
-imshow(coin_he)
 
+dime = min(radii);
+penny = 33;
+nickle = 35;
+quarter = max(radii);
+
+dime_count = 0;
+penny_count = 0;
+nickle_count = 0;
+quarter_count = 0;
+
+for i = 1:length(radii)
+    if radii(i) > penny
+        dime_count = dime_count + 1;
+    elseif radii(i) >= penny && radii(i) < nickle
+        penny_count = penny_count + 1;
+    elseif radii(i) >= nickle && radii(i) < quarter
+        nickle_count = nickle_count + 1;
+    else
+        quarter_count = quarter_count + 1;
+    end
+end
+display(dime_count)
+display(penny_count)
+display(nickle_count)
+display(quarter_count)
+value = (dime_count*10)+(penny_count)+ (nickle_count*5)+ (quarter_count*25)
+display("value = " + value+ " cents");
+    
+
+pause;
+clc; close all; clear all;
