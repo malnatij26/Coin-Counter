a9809d98b0cebb5c2cf7e51509d865fc738e6678
Harry Gordenstein <33324346+gordenstein72@users.noreply.github.com>
2020-08-01 20:11:10 -0400

    optomiozed multi coin. still could use some editing before to square out the angel

--- Coin_Counter.m
+++ Coin_Counter.m
@@ -1,10 +1,10 @@
-coins = imresize(imread("photos/good_coin.jpg"), .2);
+coins = imresize(imread("photos/good_coin.jpg"),1);
 figure; subplot(1,3,1); imshow(coins); title('Original');
 bwCoins = im2double(rgb2gray(coins));
 subplot(1,3,2); imshow(bwCoins); title('Grayscale image');
 
 %Find each coin in image using imfindcircles
-[centers, radii, metric] = imfindcircles(bwCoins, [30 90], 'ObjectPolarity','bright', 'Sensitivity',0.96, 'Method', 'TwoStage');
+[centers, radii, metric] = imfindcircles(bwCoins, [100 250], 'ObjectPolarity','bright', 'Sensitivity',0.96, 'Method', 'TwoStage');
 
 subplot(1,3,3); imshow(coins);
 
@@ -26,33 +26,49 @@ display(radii)
 
 
 dime = min(radii);
-penny = 33;
-nickle = 35;
 quarter = max(radii);
 
-dime_count = 0;
-penny_count = 0;
-nickle_count = 0;
-quarter_count = 0;
+if dime != quarter
+    %pennys diamiter now kenoing the min and max.
+    %1.14 is the diference in mm penny-dime
+    %3.3  is the diference in mm nickel-dime
+    %6.53 is the diference in mm quarter-dime
 
-for i = 1:length(radii)
-    if radii(i) > penny
-        dime_count = dime_count + 1;
-    elseif radii(i) >= penny && radii(i) < nickle
-        penny_count = penny_count + 1;
-    elseif radii(i) >= nickle && radii(i) < quarter
-        nickle_count = nickle_count + 1;
-    else
-        quarter_count = quarter_count + 1;
+    penny = ( 1.14/((6.35)/(quarter-dime)) ) + dime;
+    nickel = ( 3.3/((6.35)/(quarter-dime)) ) + dime;
+
+    dime_count = 0;
+    penny_count = 0;
+    nickel_count = 0;
+    quarter_count = 0;
+
+    for i = 1:length(radii)
+        if radii(i)< penny && (radii(i) - dime < penny - radii(i))
+            dime_count = dime_count + 1;
+        elseif radii(i) < nickel  && (radii(i) - penny < nickel - radii(i))
+            penny_count = penny_count + 1;
+        elseif radii(i) < quarter && (radii(i) - nickel < quarter - radii(i))
+            nickel_count = nickel_count + 1;
+        else
+            quarter_count = quarter_count + 1;
+        end
     end
-end
-display(dime_count)
-display(penny_count)
-display(nickle_count)
-display(quarter_count)
-value = (dime_count*10)+(penny_count)+ (nickle_count*5)+ (quarter_count*25)
-display("value = " + value+ " cents");
-    
+    display(dime_count)
+    display(penny_count)
+    display(nickel_count)
+    display(quarter_count)
+    value = (dime_count*10)+(penny_count)+ (nickel_count*5)+ (quarter_count*25);
+    display("value = " + value+ " cents");
+     
+else %single coin
 
-pause;
-clc; close all; clear all;
+    
+    
+end
+%pause;
+%clc; close all; clear all;
+
+function values = getValue()
+    values = [dime_count, penny_count, nickel_count, quarter_count, value];
+end
+
