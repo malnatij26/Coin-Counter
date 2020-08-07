224b08bf812566c40f7c18187e7dde3e8449ea55
Harry Gordenstein <33324346+gordenstein72@users.noreply.github.com>
2020-08-07 11:05:52 -0400

    small tweaks

--- single_coin.m
+++ single_coin.m
@@ -7,10 +7,10 @@ function single_coin()
 % figure; subplot(1,2,1); imshow(coins); title('Original');
 % % bwCoins = im2double(rgb2gray(coins));
 % % subplot(1,2,2); imshow(bwCoins); title('Grayscale image');
-% % 
+% %
 % %Find each coin in image using imfindcircles
 % [centers, radii, metric] = imfindcircles(coins, [100 175], 'ObjectPolarity','bright', 'Sensitivity',0.96, 'Method', 'TwoStage');
-% 
+%
 % subplot(1,2,2); imshow(coins);
 % %Show circles on original image
 % detected = viscircles(centers, radii);
@@ -22,26 +22,26 @@ end
 
 
 function match_penny(coins)
-% 
+%
 % pennies =  localcontrast(imsharpen(rgb2gray(imresize(imread("photos/p1.jpg"),1))), edgeThreshold,amount );
-% 
+%
 % coinPoints = detectSURFFeatures(coins);
 % pennyPoints = detectSURFFeatures(pennies);
-% 
+%
 % [coinFeatures, coinPoints] = extractFeatures(coins, coinPoints);
 % [pennyFeatures, pennyPoints] = extractFeatures(pennies, pennyPoints);
-% 
+%
 % coinPairs = matchFeatures(pennyFeatures, coinFeatures );
-% 
+%
 % matchedCoinPoints = coinPoints(coinPairs(:, 2), :);
 % matchedPennyPoints = pennyPoints(coinPairs(:, 1), :);
-% 
+%
 % figure;
 % showMatchedFeatures(coins, pennies, matchedCoinPoints, matchedPennyPoints, 'montage');
-% 
+%
 % title('Putatively Matched Points (Including Outliers)');
-% 
-% 
+%
+%
 % figure;
 % imshow(pennies);
 % title('300 Strongest Feature Points from penny Image');
@@ -72,69 +72,73 @@ end
 
 
 function penny_color = get_penny_color()
-
-
 pennies = imresize(imread("photos/all_p.jpg"),0.5);
-[centers, radii, metric] = imfindcircles(pennies,  [100 175], 'ObjectPolarity','bright', 'Sensitivity',0.96, 'Method', 'TwoStage');
-% disp(centers)
+[centers, radii, metric] = imfindcircles(pennies,  [130 175], 'ObjectPolarity','bright', 'Sensitivity',0.96, 'Method', 'TwoStage');
+disp(size(centers))
 cmp = colormap(copper);
 p = min(radii)*size(centers,1);
 penny_color = zeros(p,3);
 p = 1;
+
+imshow(pennies);
+%Show circles on original image
+detected = viscircles(centers, radii);
+drawnow;
+
+% Modified 8 neighbors @ a radious of x to create a list of penny color
 for x=1: min(radii)
-for i = 1 : size(centers, 1)
-   if int16(centers(i,2)) < 1500 && int16(centers(i,1)) < 1500
-       
-   RGB =  pennies( int16(centers(i,1)), int16(centers(i,2)), : );
-   penny_color(p,1)= RGB(1);
-   penny_color(p,2) = RGB(2);
-   penny_color(p,3)  = RGB(3);
-   p = p+1;
-   
-   RGB =  pennies( int16(centers(i,1))+x, int16(centers(i,2)), : );
-  penny_color(p,1)= RGB(1);
-  penny_color(p,2) = RGB(2);
-  penny_color(p,3)  = RGB(3);
-   p = p+1;
-   RGB =  pennies( int16(centers(i,1)), int16(centers(i,2))+x, : );
-     penny_color(p,1)= RGB(1);
-     penny_color(p,2) = RGB(2);
-     penny_color(p,3)  = RGB(3);
-   p = p+1;
-   RGB =  pennies( int16(centers(i,1))+x, int16(centers(i,2))+x, : );
-     penny_color(p,1)= RGB(1) ;
-     penny_color(p,2) = RGB(2) ;
-     penny_color(p,3)  = RGB(3);
-   p = p+1;
-   RGB =  pennies( int16(centers(i,1))-x, int16(centers(i,2)), : );
-     penny_color(p,1)= RGB(1);
-     penny_color(p,2) = RGB(2);
-     penny_color(p,3)  = RGB(3);
-   p = p+1;
-   RGB =  pennies( int16(centers(i,1)), int16(centers(i,2))-x, : );
-     penny_color(p,1)= RGB(1);
-     penny_color(p,2) = RGB(2);
-     penny_color(p,3)  = RGB(3);
-   p = p+1;
-   RGB =  pennies( int16(centers(i,1))-x, int16(centers(i,2))-x, : );
-     penny_color(p,1)= RGB(1);
-     penny_color(p,2) = RGB(2) ;
-     penny_color(p,3)  = RGB(3);
-   p = p+1;
-   RGB =  pennies( int16(centers(i,1))+x, int16(centers(i,2))-x, : );
-   penny_color(p,1)= RGB(1);
-   penny_color(p,2) = RGB(2) ;
-   penny_color(p,3)  = RGB(3);
-   p = p+1;
-   RGB =  pennies( int16(centers(i,1))-x, int16(centers(i,2))+x, : );
-%    penny_color = [penny_color, generateRGB(RGB)]
-    penny_color(p,1)= RGB(1) ;
-    penny_color(p,2) = RGB(2) ;
-    penny_color(p,3)  = RGB(3);
-   p = p+1;
-   end
-end
-   
+    for i = 1 : size(centers, 1)
+        if int16(centers(i,2)) < (1512-min(radii)) && int16(centers(i,1)) < (1512-min(radii)) 
+            
+            RGB =  pennies( int16(centers(i,1)), int16(centers(i,2)), : );
+            penny_color(p,1)= RGB(1);
+            penny_color(p,2) = RGB(2);
+            penny_color(p,3)  = RGB(3);
+            p = p+1;
+            
+            RGB =  pennies( int16(centers(i,1))+x, int16(centers(i,2)), : );
+            penny_color(p,1)= RGB(1);
+            penny_color(p,2) = RGB(2);
+            penny_color(p,3)  = RGB(3);
+            p = p+1;
+            RGB =  pennies( int16(centers(i,1)), int16(centers(i,2))+x, : );
+            penny_color(p,1)= RGB(1);
+            penny_color(p,2) = RGB(2);
+            penny_color(p,3)  = RGB(3);
+            p = p+1;
+            RGB =  pennies( int16(centers(i,1))+x, int16(centers(i,2))+x, : );
+            penny_color(p,1)= RGB(1) ;
+            penny_color(p,2) = RGB(2) ;
+            penny_color(p,3)  = RGB(3);
+            p = p+1;
+            RGB =  pennies( int16(centers(i,1))-x, int16(centers(i,2)), : );
+            penny_color(p,1)= RGB(1);
+            penny_color(p,2) = RGB(2);
+            penny_color(p,3)  = RGB(3);
+            p = p+1;
+            RGB =  pennies( int16(centers(i,1)), int16(centers(i,2))-x, : );
+            penny_color(p,1)= RGB(1);
+            penny_color(p,2) = RGB(2);
+            penny_color(p,3)  = RGB(3);
+            p = p+1;
+            RGB =  pennies( int16(centers(i,1))-x, int16(centers(i,2))-x, : );
+            penny_color(p,1) = RGB(1);
+            penny_color(p,2) = RGB(2) ;
+            penny_color(p,3)  = RGB(3);
+            p = p+1;
+            RGB =  pennies( int16(centers(i,1))+x, int16(centers(i,2))-x, : );
+            penny_color(p,1)= RGB(1);
+            penny_color(p,2) = RGB(2) ;
+            penny_color(p,3)  = RGB(3);
+            p = p+1;
+            RGB =  pennies( int16(centers(i,1))-x, int16(centers(i,2))+x, : );
+            penny_color(p,1)= RGB(1) ;
+            penny_color(p,2) = RGB(2) ;
+            penny_color(p,3)  = RGB(3);
+            p = p+1;
+        end
+    end
+    
 end
 
 
@@ -155,164 +159,164 @@ cmp = get_penny_color();
 
 
 for x=1: min(radii)
-for i = 1 : size(centers, 1)
-   if int16(centers(i,2)) < 1300 && int16(centers(i,1)) < 1300
-   RGB =  pennies( int16(centers(i,1)), int16(centers(i,2)), : );
-   for r = 1 : size(cmp)
-        if cmp(r)*255 == RGB(1)          
-%             disp(RGB(1)) 
-%             disp(cmp(r)*255) 
-%             disp(RGB(2)) 
-%             disp(cmp(r,2)*255)
-%             disp(cmp(r,3)*255)
-            if RGB(2)== cmp(r,2)*255          
-                disp("green")
-                if RGB(3) == cmp(r,2)*255
-                    disp(RGB)
+    for i = 1 : size(centers, 1)
+        if int16(centers(i,2)) < (1512-min(radii))  && int16(centers(i,1)) < (1512-min(radii)) 
+            RGB =  pennies( int16(centers(i,1)), int16(centers(i,2)), : );
+            for r = 1 : size(cmp)
+                if cmp(r)*255 == RGB(1)
+                    %             disp(RGB(1))
+                    %             disp(cmp(r)*255)
+                    %             disp(RGB(2))
+                    %             disp(cmp(r,2)*255)
+                    %             disp(cmp(r,3)*255)
+                    if RGB(2)== cmp(r,2)*255
+                        disp("green")
+                        if RGB(3) == cmp(r,2)*255
+                            disp(RGB)
+                        end
+                    end
+                    
                 end
             end
             
-        end
-   end
-   
-   RGB =  pennies( int16(centers(i,1))+x, int16(centers(i,2)), : );
-   for r = 1 : size(cmp)
-        if cmp(r)*255 == RGB(1) && RGB(1)~=RGB(2)          
-%             disp(RGB(1)) 
-%             disp(cmp(r)*255) 
-%             disp(RGB(2)) 
-%             disp(cmp(r,2)*255) 
-            if RGB(2)== cmp(r,2)*255          
-                disp("green")
-                if RGB(3) == cmp(r,2)*255
-                    disp(RGB)
+            RGB =  pennies( int16(centers(i,1))+x, int16(centers(i,2)), : );
+            for r = 1 : size(cmp)
+                if cmp(r)*255 == RGB(1) && RGB(1)~=RGB(2)
+                    %             disp(RGB(1))
+                    %             disp(cmp(r)*255)
+                    %             disp(RGB(2))
+                    %             disp(cmp(r,2)*255)
+                    if RGB(2)== cmp(r,2)*255
+                        disp("green")
+                        if RGB(3) == cmp(r,2)*255
+                            disp(RGB)
+                        end
+                    end
+                    
                 end
             end
             
-        end
-   end
-   
-   RGB =  pennies( int16(centers(i,1)), int16(centers(i,2))+x, : );
-   for r = 1 : size(cmp)
-        if cmp(r)*255 == RGB(1)          
-%             disp(RGB(1)) 
-%             disp(cmp(r)*255) 
-%             disp(RGB(2)) 
-%             disp(cmp(r,2)*255) 
-            if RGB(2)== cmp(r,2)*255          
-                disp("green")
-                if RGB(3) == cmp(r,2)*255
-                    disp(RGB)
+            RGB =  pennies( int16(centers(i,1)), int16(centers(i,2))+x, : );
+            for r = 1 : size(cmp)
+                if cmp(r)*255 == RGB(1)
+                    %             disp(RGB(1))
+                    %             disp(cmp(r)*255)
+                    %             disp(RGB(2))
+                    %             disp(cmp(r,2)*255)
+                    if RGB(2)== cmp(r,2)*255
+                        disp("green")
+                        if RGB(3) == cmp(r,2)*255
+                            disp(RGB)
+                        end
+                    end
+                    
                 end
             end
             
-        end
-   end
-   
-   RGB =  pennies( int16(centers(i,1))+x, int16(centers(i,2))+x, : );
-   for r = 1 : size(cmp)
-        if cmp(r)*255 == RGB(1)         
-%             disp(RGB(1)) 
-%             disp(cmp(r)*255) 
-%             disp(RGB(2)) 
-%             disp(cmp(r,2)*255) 
-            if RGB(2)== cmp(r,2)*255     
-                disp("green")
-                if RGB(3) == cmp(r,2)*255
-                    disp(RGB)
+            RGB =  pennies( int16(centers(i,1))+x, int16(centers(i,2))+x, : );
+            for r = 1 : size(cmp)
+                if cmp(r)*255 == RGB(1)
+                    %             disp(RGB(1))
+                    %             disp(cmp(r)*255)
+                    %             disp(RGB(2))
+                    %             disp(cmp(r,2)*255)
+                    if RGB(2)== cmp(r,2)*255
+                        disp("green")
+                        if RGB(3) == cmp(r,2)*255
+                            disp(RGB)
+                        end
+                    end
+                    
                 end
             end
             
-        end
-   end
-   
-   RGB =  pennies( int16(centers(i,1))-x, int16(centers(i,2)), : );
-   for r = 1 : size(cmp)
-        if cmp(r)*255 == RGB(1)          
-%             disp(RGB(1))
-%             disp(cmp(r)*255) 
-%             disp(RGB(2)) 
-%             disp(cmp(r,2)*255) 
-            if RGB(2)== cmp(r,2)*255          
-                disp("green")
-                if RGB(3) == cmp(r,2)*255
-                    disp(RGB)
+            RGB =  pennies( int16(centers(i,1))-x, int16(centers(i,2)), : );
+            for r = 1 : size(cmp)
+                if cmp(r)*255 == RGB(1)
+                    %             disp(RGB(1))
+                    %             disp(cmp(r)*255)
+                    %             disp(RGB(2))
+                    %             disp(cmp(r,2)*255)
+                    if RGB(2)== cmp(r,2)*255
+                        disp("green")
+                        if RGB(3) == cmp(r,2)*255
+                            disp(RGB)
+                        end
+                    end
+                    
                 end
             end
             
-        end
-   end
-   
-   RGB =  pennies( int16(centers(i,1)), int16(centers(i,2))-x, : );
-   for r = 1 : size(cmp)
-        if cmp(r)*255 == RGB(1)         
-%             disp(RGB(1)) 
-%             disp(cmp(r)*255) 
-%             disp(RGB(2)) 
-%             disp(cmp(r,2)*255) 
-            if RGB(2)== cmp(r,2)*255          
-                disp("green")
-                if RGB(3) == cmp(r,2)*255
-                    disp(RGB)
+            RGB =  pennies( int16(centers(i,1)), int16(centers(i,2))-x, : );
+            for r = 1 : size(cmp)
+                if cmp(r)*255 == RGB(1)
+                    %             disp(RGB(1))
+                    %             disp(cmp(r)*255)
+                    %             disp(RGB(2))
+                    %             disp(cmp(r,2)*255)
+                    if RGB(2)== cmp(r,2)*255
+                        disp("green")
+                        if RGB(3) == cmp(r,2)*255
+                            disp(RGB)
+                        end
+                    end
+                    
                 end
             end
             
-        end
-   end
-   
-   RGB =  pennies( int16(centers(i,1))-x, int16(centers(i,2))-x, : );
-   for r = 1 : size(cmp)
-        if cmp(r)*255 == RGB(1)          
-%             disp(RGB(1)) 
-%             disp(cmp(r)*255) 
-%             disp(RGB(2)) 
-%             disp(cmp(r,2)*255) 
-            if RGB(2)== cmp(r,2)*255          
-                disp("green")
-                if RGB(3) == cmp(r,2)*255
-                    disp(RGB)
+            RGB =  pennies( int16(centers(i,1))-x, int16(centers(i,2))-x, : );
+            for r = 1 : size(cmp)
+                if cmp(r)*255 == RGB(1)
+                    %             disp(RGB(1))
+                    %             disp(cmp(r)*255)
+                    %             disp(RGB(2))
+                    %             disp(cmp(r,2)*255)
+                    if RGB(2)== cmp(r,2)*255
+                        disp("green")
+                        if RGB(3) == cmp(r,2)*255
+                            disp(RGB)
+                        end
+                    end
+                    
                 end
             end
             
-        end
-   end
-   
-   RGB =  pennies( int16(centers(i,1))+x, int16(centers(i,2))-x, : );
-   for r = 1 : size(cmp)
-        if cmp(r)*255 == RGB(1)          
-%             disp(RGB(1)) 
-%             disp(cmp(r)*255) 
-%             disp(RGB(2)) 
-%             disp(cmp(r,2)*255) 
-            if RGB(2)== cmp(r,2)*255          
-                disp("green")
-                if RGB(3) == cmp(r,2)*255
-                    disp(RGB)
+            RGB =  pennies( int16(centers(i,1))+x, int16(centers(i,2))-x, : );
+            for r = 1 : size(cmp)
+                if cmp(r)*255 == RGB(1)
+                    %             disp(RGB(1))
+                    %             disp(cmp(r)*255)
+                    %             disp(RGB(2))
+                    %             disp(cmp(r,2)*255)
+                    if RGB(2)== cmp(r,2)*255
+                        disp("green")
+                        if RGB(3) == cmp(r,2)*255
+                            disp(RGB)
+                        end
+                    end
+                    
                 end
             end
             
-        end
-   end
-   
-   RGB =  pennies( int16(centers(i,1))-x, int16(centers(i,2))+x, : );
-   for r = 1 : size(cmp)
-        if cmp(r)*255 == RGB(1)          
-%             disp(RGB(1)) 
-%             disp(cmp(r)*255) 
-%             disp(RGB(2)) 
-%             disp(cmp(r,2)*255) 
-            if RGB(2)== cmp(r,2)*255          
-                disp("green")
-                if RGB(3) == cmp(r,3)*255
-                    disp(RGB)
+            RGB =  pennies( int16(centers(i,1))-x, int16(centers(i,2))+x, : );
+            for r = 1 : size(cmp)
+                if cmp(r)*255 == RGB(1)
+                    %             disp(RGB(1))
+                    %             disp(cmp(r)*255)
+                    %             disp(RGB(2))
+                    %             disp(cmp(r,2)*255)
+                    if RGB(2)== cmp(r,2)*255
+                        disp("green")
+                        if RGB(3) == cmp(r,3)*255
+                            disp(RGB)
+                        end
+                    end
+                    
                 end
             end
-            
         end
-   end
-end
-   
-end
+        
+    end
 end
 
 end
